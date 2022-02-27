f3c2d890d17de2177a52a2a07553a6e16dd75a22
rifatKasikci <78976774+rifatKasikci@users.noreply.github.com>
2022-02-27 12:36:47 +0300

    Some Configurations

--- article/forms.py
+++ article/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     class Meta():
         model = Article
         fields = ["title","content","article_image"]
+
+class CommentForm(forms.ModelForm):
+    class Meta():
+        model = Comment
+        widgets = {
+            'comment_content': forms.Textarea(attrs={'cols': 80, 'rows': 5})
+        }
+        fields = ["comment_author","comment_content"]
--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j+@u-xsyu)q(mhvm1%dkjcdg+ay%g7fao3v7$6g+)ey2ngn1sl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://rifatkasikci.pythonanywhere.com']
 
 
 # Application definition
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
